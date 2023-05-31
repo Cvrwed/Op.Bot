fdbaf7b8ea7030a826da95ebbdb1d8cbbc7ce75f
Crypt <qspoofing@gmail.com>
2023-05-31 14:58:29 -0500

    mnolo

--- OpBot.English.Version/OpBot.js
+++ OpBot.English.Version/OpBot.js
@@ -1,63 +1,78 @@
-const mineflayer = require('mineflayer')
-const { setTimeout } = require('timers')
-var AutoAuth = require('mineflayer-auto-auth')
-var crypto = require("crypto");
+const mineflayer = require('mineflayer');
+//const minecraftData = require('minecraft-data');
+//const vec3 = require('vec3');
+const crypto = require('crypto');
 
-const Bot = class {
+class Bot {
     constructor(username) {
-        const bot = mineflayer.createBot({
-            plugins: [AutoAuth], 
-            username: `${username}`,
-            AutoAuth: {password: 'OowoO777+', ignoreRepeat: true},
-            version: '1.8.9',
-            host: 'us183hake1773.ddns.net',
-            port: '25564',
-            viewDistance: 'normal',
+        this.bot = mineflayer.createBot({
+            username: username,
+            password: crypto.randomBytes(3).toString('hex'),
+            version: '1.16.5',
+            host: 'uvalubia.me',
+            port: 25565,
             hideErrors: true,
             keepAlive: true,
-            checkTimeoutInterval: 60*1000,
         });
 
-        //logs
-
-        bot.on('spawn', () => {
-            let sex = bot._client.socket;
-            console.log(`[${username}] Connect in ${sex._host}`);
+        this.bot.on('message', (message) => {
+            console.log(message.toAnsi());
         });
 
-        bot.on('disconnect', function (packet) {
-            console.log(`[${username}] Disconnect: ` + packet.reason)
+        this.bot.once('spawn', () => {
+            this.flight(); // Start flying when the bot spawns
         });
 
-        bot.on('end', () => {
-            console.log(`[${username}] Connection Lost`);
-        });
+        const pass = "Manolo123+";
+        let sessionStart = false; 
+        let registerStart = false; 
+        let tpAcceptSent = false; 
 
-        bot.on('error', (err) => { 
-            console.log(err)
-        });
-
-        //spam
-
-        bot.on('serverAuth', function () {
-            for (var i = 1; i < 100; i++) {
-                setTimeout(() => {
-                    bot.chat(`Spam by ur mom [${crypto.randomBytes(4).toString('hex')}]`);
-                }, 500)
+        this.bot.on('message', (message) => {
+            if (message.toString().includes('/login') && !sessionStart) {
+                this.bot.chat(`/login ${pass}`);
+                sessionStart = true;
+            } else if (message.toString().includes('/register') && !registerStart) {
+                this.bot.chat(`/register ${pass} ${pass}`);
+                registerStart = true;
+            } else if (message.toString().includes('/tpaccept') && !tpAcceptSent) {
+                this.bot.chat('/tpaccept');
+                tpAcceptSent = true;
             }
         });
+
+
+        this.bot.on('disconnect', (packet) => {
+            console.log(`[${username}] Disconnected: ` + packet.reason);
+        });
+
+        this.bot.on('end', () => {
+            console.log(`[${username}] Connection Lost.`);
+        });
+
+        this.bot.on('error', (err) => {
+        console.log(err);
+        });
     }
-};
-        //Join Bots
-let i = 0
-function next() {
-    if (i < 200) {
-        i++
-        setTimeout(() => {
-            new Bot(crypto.randomBytes(8).toString('hex'));
-            next()
-        }, 1000)
+
+    flight() {
+        this.bot.creative.startFlying(true);
+        this.bot.setControlState('jump', true);
+    }
+
+    joinBots(numBots) {
+        let i = 0;
+        const interval = setInterval(() => {
+        if (i < numBots) {
+            i++;
+            const username = crypto.randomBytes(8).toString('hex');
+            new Bot(username);
+        } else {
+            clearInterval(interval);
+        }
+        }, 5000);
     }
 }
 
-next()
+const numBots = 200;
+new Bot('Tzt251').joinBots(numBots);
